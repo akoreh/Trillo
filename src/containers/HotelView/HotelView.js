74152828115ac9e49c2b41923fc608c55004b4bc
Koreh Andrei <koreh.andrei20@gmail.com>
2018-08-18 10:50:59 +0300

    Added new components to the hotelview

--- src/containers/HotelView/HotelView.js
+++ src/containers/HotelView/HotelView.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import './HotelView.css';
+import Image1 from '../../assets/img/hotel-1.jpg';
+import Image2 from '../../assets/img/hotel-2.jpg';
+import Image3 from '../../assets/img/hotel-3.jpg';
+
+import Gallery from '../../components/Gallery/Gallery';
+import Overview from '../../components/Overview/Overview';
 
 const hotelView = () => {
 
     return (
         <main className="hotel-view">
-            Hotel view
+            <Gallery images={[Image1, Image2, Image3]}/>
+            <Overview />
         </main>
     );
 }
